fa2b436cb6509fd599b45b76872873e7be30f7b5
robinsinghai <robin.rs1414@gmail.com>
2025-10-26 09:26:58 +0530

    removed old fil

--- langchain_anthropic_api.py
+++
@@ -1,8 +0,0 @@
-from langchain_anthropic import ChatAnthropic
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatAnthropic(model_name="claude-3-5-sonnet-20241022", timeout=None, stop=None)
-result = model.invoke("Write a sonnet about the sea.")
-print(result.content)
